0d77d23b0bf054ea0356120ca273d9ff3a93eab3
Millicent Adjubel <adjubelmillicent@gmail.com>
2023-05-25 22:33:46 +0000

    Assignment3

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,18 @@
+let myName="Millicent";
+
+typeof myName;
+
+myName="Adjubel";
+
+let car={
+    name:"Corolla",
+    type:"Toyota",
+    year:2023,
+    colour:"black",
+    weight:"100kg",
+    number:1234,
+    isFast:true,
+    isStrong:false,
+    height:"1234mm",
+    power:"234bhp"
+}
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,11 @@
+const favoriteNumber= 12;
+
+const halfofFavNumber= favoriteNumber /2;
+
+let IsaacsFavorite =13;
+
+IsaacsFavorite= IsaacsFavorite -favoriteNumber;
+
+IsaacsFavorite=IsaacsFavorite * 26;
+
+IsaacsFavorite= IsaacsFavorite % 3;
